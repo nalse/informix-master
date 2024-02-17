a3f7109dacd8e2330ccd5000a57095aa0811aaf5
nalse <54538144+nalse@users.noreply.github.com>
2024-02-18 02:57:38 +0400

    Update settings.py
    DB Update

--- informix/settings.py
+++ informix/settings.py
@@ -1,28 +1,12 @@
-
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-u(3@eu$9scz8rm$=h#4k3-rzlq0d)75h^=uan4))=ddw+8g6w+'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-
-DEBUG = True 
-ALLOWED_HOSTS = ['*']
-
 DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
@@ -44,20 +28,16 @@ INSTALLED_APPS = [
     'questions',
     'results',
 ]
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-     'whitenoise.middleware.WhiteNoiseMiddleware',
-    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     
 ]
 
 ROOT_URLCONF = 'informix.urls'
@@ -65,7 +45,7 @@ ROOT_URLCONF = 'informix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,25 +60,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'informix.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'informix',
-        'USER': 'informix',
-        'PASSWORD': 'informix2000@',
-        'HOST': 'localhost',   # Set to your MySQL server host
-        'PORT': '3306',        # Set to your MySQL server port
+        'NAME': 'defaultdb',
+        'USER': 'doadmin',
+        'PASSWORD': 'AVNS_QUtLQqyN6-UvfAXwVWZ',
+        'HOST': 'informix-db-do-user-8043912-0.c.db.ondigitalocean.com',
+        'PORT': '25060',
     }
 }
-
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -115,53 +86,31 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static/',
-    BASE_DIR / 'quizes' /'static',
-]
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    BASE_DIR / 'quizes' /'static',
+    BASE_DIR / 'quizes' / 'static',
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-CKEDITOR_UPLOAD_PATH = 'uploads/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#media file
-MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-#message file
 from django.contrib.messages import constants as messages
-
 MESSAGE_TAGS = {
     messages.INFO: "danger",
 }
-# django_project/settings.py
+
 LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = "index"
 
@@ -170,5 +119,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'yavuzadpu@gmail.com'
-EMAIL_HOST_PASSWORD ='xuudkumwhhvezrkv'
-
+EMAIL_HOST_PASSWORD = 'xuudkumwhhvezrkv'
